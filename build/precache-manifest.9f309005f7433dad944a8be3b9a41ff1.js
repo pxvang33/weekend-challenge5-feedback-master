76a6ed9fae49fb7a77820554b92b541775cd69c5
Peng Xue Vang <pxvang33@gmail.com>
2019-01-28 16:01:24 -0600

    Added build folder for Heroku.

---
+++ build/precache-manifest.9f309005f7433dad944a8be3b9a41ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5dd0dce0d227b8f308e",
+    "url": "/static/css/main.c297c0cb.chunk.css"
+  },
+  {
+    "revision": "c5dd0dce0d227b8f308e",
+    "url": "/static/js/main.c5dd0dce.chunk.js"
+  },
+  {
+    "revision": "bab918649aa48418842e",
+    "url": "/static/js/1.bab91864.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d45cd0a9c46b3cab81b587e9f9ea31f0",
+    "url": "/index.html"
+  }
+];
